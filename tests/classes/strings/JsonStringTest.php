2c7bdfa334761fc8ab65851007b258fd505fd434
sevidmusic <sevidmusic@gmail.com>
2023-03-05 04:49:36 -0500

    Updated composer.
    Prepared the following files for development:
    
    ````
    src/classes/strings/JsonSerializedObject.php
    src/classes/strings/JsonString.php
    src/interfaces/strings/JsonSerializedObject.php
    src/interfaces/strings/JsonString.php
    tests/PHPJsonUtilitiesTest.php
    tests/classes/strings/JsonSerializedObjectTest.php
    tests/classes/strings/JsonStringTest.php
    tests/interfaces/strings/JsonSerializedObjectTestTrait.php
    tests/interfaces/strings/JsonStringTestTrait.php
    
    ```

--- tests/classes/strings/JsonStringTest.php
+++ tests/classes/strings/JsonStringTest.php
@@ -1,2 +1,21 @@
 <?php
 
+namespace Darling\PHPJsonUtilities\tests\classes\strings;
+
+use Darling\PHPJsonUtilities\classes\strings\JsonString;
+use Darling\PHPJsonUtilities\tests\interfaces\strings\JsonStringTestTrait;
+use tests\classes\strings\TextTest;
+
+class JsonStringTest extends TextTest
+{
+
+    use JsonStringTestTrait;
+
+    protected function setUp(): void
+    {
+        $string = $this->randomChars();
+        $this->setExpectedString($string);
+        $this->setTextTestInstance(new JsonString($string));
+    }
+
+}
