10a3068f0f9f8f05ce9326b282e1f3e8479e696a
sevidmusic <sevidmusic@gmail.com>
2023-07-03 12:12:03 -0400

    Added new examples

---
+++ examples/exampleOfEncodingAndDecodingAnInt.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file demonstrates how to use a Json instance to encode
+ * an int as json.
+ *
+ * This example should be run from this library's examples directory.
+ *
+ * For example:
+ *
+ * ```
+ * php ./examples/exampleOfEncodingAnInt.php
+ *
+ * ```
+ *
+ */
+
+require_once(
+    str_replace('examples' , '', __DIR__) .
+    DIRECTORY_SEPARATOR .
+    'vendor' .
+    DIRECTORY_SEPARATOR .
+    'autoload.php'
+);
+
+use \Darling\PHPJsonUtilities\classes\encoded\data\Json;
+use \Darling\PHPMockingUtilities\classes\mock\values\MockInt;
+
+/**
+ * Example of encoding an int:
+ */
+$int = new MockInt();
+
+$jsonEncodedInt = new Json($int->value());
+
+echo $jsonEncodedInt . PHP_EOL;
+
+/**
+ * example output:
+ *
+ * 2609743170528575717
+ *
+ */
+
+//decoder
---
+++ examples/exampleOfEncodingAndDecodingAnObjectInstanceThatDefinesReadonlyProperties.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This file demonstrates how to use a Json instance to encode an
+ * object instance that defines readonly properties as json.
+ *
+ * This example should be run from this library's examples directory.
+ *
+ * For example:
+ *
+ * ```
+ * php ./examples/exampleOfEncodingAnObjectInstanceThatDefinesReadonlyProperties.php
+ *
+ * ```
+ *
+ */
+
+require_once(
+    str_replace('examples' , '', __DIR__) .
+    DIRECTORY_SEPARATOR .
+    'vendor' .
+    DIRECTORY_SEPARATOR .
+    'autoload.php'
+);
+
+use \Darling\PHPJsonUtilities\classes\encoded\data\Json;
+
+class DefinesReadonlyProperties
+{
+
+    public function __construct(private readonly int $int) {}
+
+    public function getInt(): int
+    {
+        return $this->int;
+    }
+}
+
+/**
+ * Example of encoding an object that defines readonly properties:
+ */
+$objectInstance = new DefinesReadonlyProperties(10);
+
+$jsonEncodedObject = new Json($objectInstance);
+
+echo $jsonEncodedObject . PHP_EOL;
+
+/**
+ * example output:
+ *
+ * {"__class__":"DefinesReadonlyProperties","__data__":{"int":10}}
+ *
+ */
+
+//decoder
